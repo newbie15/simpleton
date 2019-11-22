48f9563a25bb9f5f93c8cae3fba37e13a00c5d26
newbie15 <fajar.rukmo@gmail.com>
2019-11-22 15:45:42 +0700

    icon menu aplikasi

--- application/modules/main/views/main_menu.php
+++ application/modules/main/views/main_menu.php
@@ -1,12 +1,15 @@
 <li class="header">Main Menu</li>
 
-<li <?php if(isset($m_penjualan)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/penjualan')?>'><i class="fa fa-circle-o"></i> <span>Penjualan</span></a></li>
-<li <?php if(isset($m_stok)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/stok')?>'><i class="fa fa-circle-o"></i> <span>Stok</a></span></li>
-<li <?php if(isset($m_user)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/user')?>'><i class="fa fa-circle-o"></i> <span>User</a></span></li>
-<li <?php if(isset($m_karyawan)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/karyawan')?>'><i class="fa fa-circle-o"></i> <span>Karyawan</a></span></li>
-<li <?php if(isset($m_kartu)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/kartu')?>'><i class="fa fa-circle-o"></i> <span>Kartu Anggota</a></span></li>
+<li <?php if(isset($m_penjualan)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/penjualan')?>'><i class="fa fa-shopping-cart"></i> <span>Penjualan</span></a></li>
+<li <?php if(isset($m_stok)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/stok')?>'><i class="fa fa-dropbox"></i> <span>Stok</a></span></li>
 
-<li <?php if(isset($m_ustok)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/update_data')?>'><i class="fa fa-circle-o"></i> <span>Update Data</a></span></li>
-<li <?php if(isset($m_ustok)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/rekap_penjualan')?>'><i class="fa fa-circle-o"></i> <span>Rekap Penjualan</a></span></li>
 
-<li <?php if(isset($m_tiket)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/tiket')?>'><i class="fa fa-circle-o"></i> <span>Tiket Travel</a></span></li>
+<li <?php if(isset($m_user)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/user')?>'><i class="fa fa-user"></i> <span>User</a></span></li>
+<li <?php if(isset($m_karyawan)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/karyawan')?>'><i class="fa fa-users"></i> <span>Karyawan</a></span></li>
+<li <?php if(isset($m_kartu)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/kartu')?>'><i class="fa fa-credit-card"></i> <span>Kartu Anggota</a></span></li>
+<li <?php if(isset($m_kartu)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/kamera')?>'><i class="fa fa-camera"></i> <span>Foto Anggota</a></span></li>
+
+<li <?php if(isset($m_ustok)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/update_data')?>'><i class="fa fa-upload"></i> <span>Update Data</a></span></li>
+<li <?php if(isset($m_ustok)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/rekap_penjualan')?>'><i class="fa fa-line-chart"></i> <span>Rekap Penjualan</a></span></li>
+
+<li <?php if(isset($m_tiket)) { ?>class = "active" <?php } ?>><a href='<?php echo site_url('main/examples/tiket')?>'><i class="fa fa-plane"></i> <span>Tiket Travel</a></span></li>
