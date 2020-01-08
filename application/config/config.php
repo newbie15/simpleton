484df60410c5a0de6d13572ff083df75007319ce
newbie15 <fajar.rukmo@gmail.com>
2020-01-08 17:49:48 +0700

    remove index in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] . str_replace(basename
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
