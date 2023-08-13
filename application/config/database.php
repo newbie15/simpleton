9ce88c17fece8d2252d8800879743f6961be4254
KMS manager <fajar.rukmo@gmail.com>
2023-08-13 22:23:02 +0700

    database ke server

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '192.168.5.85',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'database' => 'simpleton',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
